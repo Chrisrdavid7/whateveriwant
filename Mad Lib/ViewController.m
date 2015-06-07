05ab35fa0b064fd683a4488d88cc6779b274ddb8
Chrisrdavid7 <chrisrdavid7@gmail.com>
2015-06-07 18:03:53 -0400

    Fifth Commit

--- Mad Lib/ViewController.m
+++ Mad Lib/ViewController.m
@@ -15,12 +15,20 @@
 @property (weak, nonatomic) IBOutlet UITextField *place;
 @property (weak, nonatomic) IBOutlet UITextField *body;
 @property (weak, nonatomic) IBOutlet UITextField *song;
+@property (weak, nonatomic) IBOutlet UITextView *story;
 @property (weak, nonatomic) IBOutlet UITextField *color;
+@property (weak, nonatomic) IBOutlet UIButton *Hit;
 @end
 
+
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.story.hidden = YES;
+}
 - (IBAction)buttonpress:(id)sender {
-    
     NSString *name = self.name.text;
     NSString *noun = self.noun.text;
     NSString *food = self.food.text;
@@ -29,16 +37,27 @@
     NSString *song = self.song.text;
     NSString *color = self.color.text;
     
+    self.name.hidden = YES;
+    self.noun.hidden = YES;
+    self.food.hidden = YES;
+    self.place.hidden = YES;
+    self.body.hidden = YES;
+    self.song.hidden = YES;
+    self.color.hidden = YES;
+    self.story.hidden = YES;
     
-    NSLog(@"%@ is at %@, eating %@. %@ has been %@'s favorite place to visit, full of broken %@'s. %@ listens to %@ while swimming in the %@ lake.", name, noun, food, place, name, body, name, song, color);
-    // Hit Command - K to hide on screen keyboard. 
+    self.story.text = [NSString stringWithFormat:@"%@ is at %@, eating %@. %@ has been %@'s favorite place to visit, full of broken %@'s. %@ listens to %@ while swimming in the %@ lake.", name, noun, food, place, name, body, name, song, color];
+
+    // Hit Command - K to hide on screen keyboard.
+   
+    
+    
+    self.story.hidden = NO;
+    self.Hit.hidden = YES;
     
     
 }
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
